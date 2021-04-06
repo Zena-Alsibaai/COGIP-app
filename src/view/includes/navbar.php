0758d9872938aaa25f865984ea36c0b2d77485ac
Chardon François <francoischardon.contact@gmail.com>
2021-04-06 13:53:41 +0200

    commun

--- src/view/includes/navbar.php
+++ src/view/includes/navbar.php
@@ -5,21 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GOGIP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../../style.css">
 </head>
 <body>
     
 <nav class="header">
 <div class="logoOutline">
 <div class="logoImage">
-GOGIB </br> LOGO
+<p> GOGIB </br> LOGO </p>
 </div>
 </div>
 
 <div class="placementLink">
 <div class="linkMenu"><a class='link' href="./homePage.php">Home</a></div>
-<div class="linkMenu"><a class='link' href="./invoicesPage.php">Invoices</a></div>
+<div class="linkMenu"><a class='link' href="./invoicesPage2.php">Invoices</a></div>
 <div class="linkMenu"><a class='link' href="./companiesPage.php">Companies</a></div>
 <div class="linkMenu"><a class='link' href="./clientsPage.php">Contacts</a></div>
 <div class="linkMenu"><a class="link" href="#">Inscription</a></div>
