2ec7f444bea9ae421452c94dce841a4294f8ac3d
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-04-19 16:30:23 +0200

    Delete qqch

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    echo"coucou famille bot, aujourd'hui c'est la fin de COGIP, et c'est du CACA";
+    echo"coucou famille bot, aujourd'hui c'est la fin de COGIP";
     // declare(strict_types=1);
     require './src/view/companiesPage.php';
 
