d02ec3011a37816e8efe9dc73851abd46b71bf20
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-04-19 16:07:03 +0200

    Change require2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-    echo"coucou famille bot, aujourd'hui c'est la fin de COGIP";
+    echo"coucou famille bot, aujourd'hui c'est la fin de COGIP, et c'est du CACA";
     // declare(strict_types=1);
-    require './src/view/homePage.php';
+    require './src/view/companiesPage.php';
 
 ?>
